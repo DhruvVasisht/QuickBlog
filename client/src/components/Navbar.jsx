68202f1feb5d8acb18a5099e88ebd3e30811bd19
DhruvVasisht <vasisht.dhruv7@gmail.com>
2025-06-01 00:28:02 +0530

    Start

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer">
+        <img src={logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
