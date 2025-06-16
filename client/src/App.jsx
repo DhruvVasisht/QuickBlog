dfeca7e475f7ad83bb2de13a29e30e02aafd1d01
DhruvVasisht <vasisht.dhruv7@gmail.com>
2025-06-16 17:20:05 +0530

    Admin page started

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Blog from './pages/Blog'
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import ListBlog from "./pages/admin/ListBlog";
+import AddBlog from "./pages/admin/AddBlog";
+import Comment from "./pages/admin/Comment";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/blog/:id' element={<Blog />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/blog/:id" element={<Blog />} />
+      <Route path="/admin" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="list-blog" element={<ListBlog />} />
+        <Route path="add-blog" element={<AddBlog />} />
+        <Route path="comments" element={<Comment />} />
+      </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/admin/Layout.jsx
@@ -0,0 +1,24 @@
+import { useNavigate, Outlet } from 'react-router-dom'
+import { assets } from '../../assets/assets';
+
+
+const Layout = () => {
+  const navigate = useNavigate();
+  const logout = () => {
+    navigate("/");
+  }
+  return (
+    <>
+      <div className='flex items-center justify-between p-2 h-[70px] px-4 sm:px-12 border-b border-gray-200'>
+        <img src={assets.logo} alt="" className='w-32 sm:w-40 cursor-pointer' onClick={()=>navigate("/")} />
+        <button onClick={logout} className="text-sm px-8 py-2 bg-primary text-white rounded-full cursor-pointer">Logout</button>
+      </div>
+      <div className='flex h-[calc(100vh-70px)]'>
+        <div>SideBar</div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
+
+export default Layout
